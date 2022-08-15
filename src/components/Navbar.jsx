5d5e5679439044005f3c43b39f079679d2595b7f
Manohar B <gowreemanohar@gmail.com>
2022-08-15 17:30:53 +0530

    responsive+Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState}from 'react'
 import Links from './Links';
+import {ImLink} from "react-icons/im"
 const Navbar = () => {
   const [account,setAccount]=useState(false);
   const handleClick=(e)=>{
@@ -13,11 +14,11 @@ const Navbar = () => {
     <div>
     <div className=" bg-blue-500  rounded-lg   z-40 fixed top-0 md:w-screen w-screen">
         <div className="text-white text-lg font-bold flex justify-around shadow-lg shadow-blue-400/50 p-5 relative">  
-               <p className=''>
+               <p className='text-3xl text-bold drop-shadow-xl'>
                     GFG Tree Visualizer
                 </p>
                 <p>
-                    <button  onClick={handleClick}>Links</button>
+                    <button className="text-2xl text-bold" onClick={handleClick}><ImLink color="bold" fontWeight="bold"/></button>
                 </p>
                 
         </div> 
